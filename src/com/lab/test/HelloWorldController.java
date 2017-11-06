81996875ada99fe9399a55b981ba37895e3ba919
154070 <154070@INDSBLRJ2130J2.AMERICAS.GLOBAL.NTTDATA.COM>
2017-11-06 17:44:15 +0530

    Test

--- src/com/lab/test/HelloWorldController.java
+++ src/com/lab/test/HelloWorldController.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 
 public class HelloWorldController {
-
+//Just For the demo
 	@RequestMapping("/hello")
 	public ModelAndView helloworld()
 	{
